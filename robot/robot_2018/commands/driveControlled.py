cdfd37d9836185a44dabffe0f3772129c8f5c7e6
JABurindaPersonal <35407472+JABurindaPersonal@users.noreply.github.com>
2018-01-19 18:34:08 -0700

    Add files via upload

--- robot/robot_2018/commands/driveControlled.py
+++ robot/robot_2018/commands/driveControlled.py
@@ -6,14 +6,15 @@ from robotMap import robotMap
 class DriveControlled(command.Command):
     def __init__(self):
         super().__init__('DriveControlled')
-        self.controller1=robotMap.controllerMap.controller1
-        self.controller2=robotMap.controllerMap.controller2
+       # self.controller1=robotMap.controllerMap.controller1
+        #self.controller2=robotMap.controllerMap.controller2
         # old way self.controller2=wpilib.Joystick(map.joystickmap.controller2)
         self.requires(self.getRobot().drivetrain)
     
     def execute (self):
-        x= self.controller1.getX()
-        y= self.controller1.getY()
-        rot= self.controller1.getZ()
-     
+        #if(isOperatorControl() and isEnabled())
+        myController = self.getRobot().controller1        
+        x= myController.getX(hand =None)
+        y= myController.getY()
+        rot= myController.getZ()
         self.getRobot().drivetrain.move(x,y,rot)
