530620ea813f6e9bec263a402b425575ebb7e428
tuffinmuffin <tuffinmuffin@gmail.com>
2018-02-10 15:25:41 -0700

    added lifter code

---
+++ robot/robot_2018/commands/lifterCommand.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+
+import wpilib
+from wpilib import command
+from wpilib import Encoder
+from wpilib import XboxController
+
+class lifterCommand(command.Command):
+    def __init__(self):
+        super().__init__('LifterCommand')
+        
+        self.requires(self.getRobot().lifter)
+        self.getRobot().smartDashboard.putNumber("lifterSpeed", -1)
+        self.controllerMap=self.getRobot.robotMap.controllerMap
+        
+    def execute (self):
+        spd = self.getSpeed()
+        self.getRobot().lifter.move(spd)
+        self.getRobot().smartDashboard.putNumber("lifterSpeed", spd)
+        
+    def getSpeed(self):
+        controller = self.getRobot().driveController
+        upSpeed=controller.getRawAxis(self.controllerMap.driveController['lifterUpAxis'])
+        downSpeed=controller.getRawAxis(self.controllerMap.driveController['lifterDownAxis'])
+        spdscale=self.getRobot().smartDashboard.getNumber("lifterSpeed", -1)
+        deadZone=self.getRobot().smartDashboard.getNumber("deadZone", 0.1) 
+        if (upSpeed>=deadZone and downSpeed>=deadZone):
+            return 0 
+        if (upSpeed>=deadZone):
+            return upSpeed*spdscale
+        if (downSpeed>=deadZone):
+            return -downSpeed*spdscale
+        return 0 
+        
